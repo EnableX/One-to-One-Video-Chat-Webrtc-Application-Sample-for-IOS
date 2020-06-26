e3d0436a509ce8ebf1fb43342a3eca22c6ba387b
jaykumarvcx <jayprakash.k@vcloudx.com>
2020-06-26 20:48:43 +0530

    Update readme files and change server end point

--- sampleiOS/sampleiOS/ServiceClass/VCXServicesClass.swift
+++ sampleiOS/sampleiOS/ServiceClass/VCXServicesClass.swift
@@ -25,8 +25,10 @@ class VCXServicesClass: NSObject {
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("application/json", forHTTPHeaderField: "Accept")
-        //Create Base64 Encription
-        request.addValue("Basic \(createBase64encoding())", forHTTPHeaderField: "Authorization")
+        if(kTry){
+             request.addValue(kAppId, forHTTPHeaderField: "x-app-id")
+            request.addValue(kAppkey, forHTTPHeaderField: "x-app-key")
+        }
         //create dataTask using the session object to send data to the server
         let tast = session.dataTask(with: request as URLRequest){(data,response, error) in
             guard error == nil else{
@@ -69,12 +71,6 @@ class VCXServicesClass: NSObject {
         tast.resume()
     }
     
-    class func createBase64encoding() ->String{
-        let headerString = String(format: "%@:%@",userName ,password)
-        let date = headerString.data(using: .utf8)
-        return date!.base64EncodedString(options: NSData.Base64EncodingOptions(rawValue: 0))
-    }
-    
     // MARK: - Join Room With Room ID
     /**
     Input Parameter : - RoomId
@@ -89,6 +85,10 @@ class VCXServicesClass: NSObject {
         request.httpMethod = "GET"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("application/json", forHTTPHeaderField: "Accept")
+        if(kTry){
+             request.addValue(kAppId, forHTTPHeaderField: "x-app-id")
+             request.addValue(kAppkey, forHTTPHeaderField: "x-app-key")
+        }
         //create dataTask using the session object to send data to the server
         let tast = session.dataTask(with: request as URLRequest){(data,response, error) in
             guard error == nil else{
@@ -132,58 +132,7 @@ class VCXServicesClass: NSObject {
         }
         tast.resume()
     }
-    /*
-    // MARK: - Join Room With Pin
-    /**
-     Input Parameter : - Pin Nummer
-     Return :- VCXRoomInfoModel
-     **/
-    class func fetchRoomInfoWithPin(parameter : String , completion:@escaping (VCXRoomInfoModel) -> ()){
-        let param = ["pin" : parameter]
-        //create the url with URL
-        let url = URL(string: kBasedURL + "getRoomByPin")!
-        //Create A session Object
-        let session = URLSession.shared
-        //Now create the URLRequest object using the url object
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        do{
-            request.httpBody = try JSONSerialization.data(withJSONObject: param, options:.prettyPrinted)
-        } catch let error {
-            print(error.localizedDescription)
-        }
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        //create dataTask using the session object to send data to the server
-        let tast = session.dataTask(with: request as URLRequest){(data,response, error) in
-            guard error == nil else{
-                let roomdataModel = VCXRoomInfoModel()
-                roomdataModel.error = error
-                completion(roomdataModel)
-                return}
-            guard let data = data else {
-                let roomdataModel = VCXRoomInfoModel()
-                roomdataModel.isRoomFlag = false
-                completion(roomdataModel)
-                return}
-            do{
-                if let responseValue = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String : String]{
-                    let roomdataModel = VCXRoomInfoModel()
-                    roomdataModel.role = responseValue["role"]
-                    roomdataModel.room_id = responseValue["room_id"]
-                    roomdataModel.mode = responseValue["mode"]
-                    roomdataModel.isRoomFlag = true
-                    completion(roomdataModel)
-                }
-            }catch{
-                let roomdataModel = VCXRoomInfoModel()
-                roomdataModel.error = error
-                completion(roomdataModel)
-                print(error.localizedDescription)
-            }
-        }
-        tast.resume()
-    }*/
+    
     // MARK: - featchToken
     /**
      Input Parameter : - [String : String]
@@ -204,6 +153,10 @@ class VCXServicesClass: NSObject {
         }
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("application/json", forHTTPHeaderField: "Accept")
+        if(kTry){
+             request.addValue(kAppId, forHTTPHeaderField: "x-app-id")
+             request.addValue(kAppkey, forHTTPHeaderField: "x-app-key")
+        }
         //create dataTask using the session object to send data to the server
         let tast = session.dataTask(with: request as URLRequest){(data,response, error) in
             guard error == nil else{
